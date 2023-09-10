376fc4143ec80c53e663c5a65fb5f6ca65006248
Luc Hondareyte <luc.hondareyte@laposte.net>
2023-09-10 17:57:54 +0200

    Add LED definition

--- src/io.h
+++ src/io.h
@@ -9,6 +9,8 @@
 #define FIRE_SW   1
 #define IN        2
 #define OUT       3
+#define LED       4
 #define PORT_SW   PINB
 #define MIDI_IN   _SFR_IO_ADDR(PORTB),IN
 #define MIDI_OUT  _SFR_IO_ADDR(PORTB),OUT
+#define LED_OUT   _SFR_IO_ADDR(PORTB),LED
